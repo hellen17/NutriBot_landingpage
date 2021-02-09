107cd0b5a1a2e35efc035bf70d153f0f365762eb
Hellen Kokach <42536943+hellen17@users.noreply.github.com>
2021-02-09 21:29:40 +0300

    Added login and signup page

--- rasaweb/urls.py
+++ rasaweb/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
 
     path('',views.home, name='index'),
     path('chatroom/', views.chatroom, name='chatroom'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
   
 ]
--- rasaweb/views.py
+++ rasaweb/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -8,4 +10,20 @@ def home(request):
 def chatroom(request):
    return render(request, 'chatroom.html')
 
- 
+def login(request):
+   return render(request, 'login.html')
+
+def register(request):
+   if request.method == 'POST':
+      form = UserCreationForm(request.POST)
+      if form.is_valid():
+         form.save()
+         username = form.cleaned_data.get('username')
+         messages.success(request, f'Account created for {username}!')
+         return redirect('chatroom')
+   else:
+      form = UserCreationForm()   
+   form = UserCreationForm()
+   return render(request, 'register.html', {'form':form})  
+
+
